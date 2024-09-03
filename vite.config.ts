b0152b1762e38339291b704b8bb7aa3ee23ef345
HGZdevi <HGZdevi@gmail.com>
2024-09-04 00:19:02 +0200

    rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import {defineConfig} from "vitest/config";
 import react from "@vitejs/plugin-react";
 import {getViteConfig} from "./src/_server/helpers.ts";
+import {visualizer} from "rollup-plugin-visualizer";
 
 export default ({mode}) => {
   const {VITE_LOCAL_PORT, VITE_LOCAL_SERVER_PORT} = getViteConfig(mode);
 
   return defineConfig({
-    plugins: [react()],
+    plugins: [
+      react(),
+      visualizer({
+        filename: `./build/stats.html`,
+        // open: true,
+        gzipSize: true,
+        brotliSize: true,
+      }) as PluginOption,
+    ],
     server: {
       port: parseInt(VITE_LOCAL_PORT, 10),
     },
